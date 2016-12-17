110483073e08816e4bbfbc1969038b212ade9ad7
Brian Chan <brian.chan10852@yahoo.com>
2016-12-16 23:44:42 -0500

    fix filename scheme, epoch time now works when at 11pm, add sleep 1s after killing process to allow release of recording resources

--- record.sh
+++ record.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
+#catch ctl-c and stop the record process before exit
+trap stopScript INT
 
+function stopScript() {
+  echo "stopping script"
+  kill $pid  
+  exit 1
+}
 
 while [ true ]
 do
+  #secToRecord=5
+  secToRecord=$(( 3600 - $(date +%s) % 3600 ))
   epochStartTime=$(date '+%s')
-  epochEndTime=$(date -d $(date -d 'next hour' '+%H:00:00') '+%s')
-  dateStartTime=$(date -d@$epochStartTime '+%m.%d.%Y_%H:%M')
-  dateEndTime=$(date -d@$epochEndTime '+%m.%d.%Y_%H:%M')
-  secToRecord=`expr $epochEndTime - $epochStartTime`
-  filename=$dateStartTime'--'$dateEndTime
+  epochEndTime=`expr $secToRecord + $epochStartTime`
+  dateStartTime=$(date -d@$epochStartTime '+%m.%d.%Y_%H.%M.%S')
+  dateEndTime=$(date -d@$epochEndTime '+%m.%d.%Y_%H.%M.%S')
+  fileName=$dateStartTime'--'$dateEndTime
 
   echo "$fileName"
   echo "$dateStartTime"
   echo "$dateEndTime"
   echo "$secToRecord"
   
+  #initiate the recording for specified time
   recordmydesktop --no-cursor --on-the-fly-encoding -o $fileName &
+  #echo "sleeping"
   sleep $secToRecord
+  #echo "awake"
   pid=$!
+  #killing the pid stops AND saves the recording
   kill $pid
+  sleep 1
 done
