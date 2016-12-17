93ad070fdfb55150094253a6eb65f96d6f0aeaaa
Brian Chan <brian.chan10852@yahoo.com>
2016-12-16 22:40:01 -0500

    pretty format date and filename; added record program command

--- record.sh
+++ record.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 
+
 while [ true ]
 do
   epochStartTime=$(date '+%s')
   epochEndTime=$(date -d $(date -d 'next hour' '+%H:00:00') '+%s')
-  dateStartTime=$(date -d@$epochStartTime)
-  dateEndTime=$(date -d@$epochEndTime)
+  dateStartTime=$(date -d@$epochStartTime '+%m.%d.%Y_%H:%M')
+  dateEndTime=$(date -d@$epochEndTime '+%m.%d.%Y_%H:%M')
   secToRecord=`expr $epochEndTime - $epochStartTime`
-  
+  filename=$dateStartTime'--'$dateEndTime
+
+  echo "$fileName"
   echo "$dateStartTime"
   echo "$dateEndTime"
   echo "$secToRecord"
-  #recordmydesktop  system:capture_1
-  sleep 1
+  
+  recordmydesktop --no-cursor --on-the-fly-encoding -o $fileName &
+  sleep $secToRecord
+  pid=$!
+  kill $pid
 done
